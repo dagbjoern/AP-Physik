78010fb78cf4ce099dbe8de5edbeda6e122af251
Dag-Björn Hering <dag.hering@udo.edu>
2016-06-12 16:18:56 +0200

    ein paar korrekturen für V704 und V605

--- V703/auswertung.py
+++ V703/auswertung.py
@@ -12,10 +12,12 @@ from uncertainties import ufloat
 U , n_10 , I=np.genfromtxt('a.txt',unpack=True)
 U_lin , n_lin , I_lin=np.genfromtxt('a.txt',skip_header=3,skip_footer=2,unpack=True)
 
-n=n_10/10 #Zählrate pro secunde
 n_lin=n_lin/10
+n=n_10/10
+n_10=unp.uarray(n_10,np.sqrt(n_10))
+N=n_10/10
+ #Zählrate pro secunde
 I=unp.uarray(I*10**(-6),0.000002)
-N=unp.uarray(n,np.sqrt(n))
 print(N)
 def fehler_b(std_m,x):
     return(std_m*np.sqrt(np.mean(x**2)))
@@ -27,28 +29,28 @@ B=unp.uarray(b,fehler_b(std,U_lin))
 #Abgeschätzter fehler  für Zähler 10
 
 plt.figure(1)
-plt.errorbar(U ,n,xerr=0,yerr=np.sqrt(n), fmt='cx')
+plt.errorbar(U ,n,xerr=0,yerr=stds(N), fmt='cx')
 plt.plot(U,n,'gx',label=r'$\mathrm{Messwert \ außerhalb \ des \ Plateaus}$')
 plt.plot(U_lin,n_lin,'bx',label=r'$\mathrm{Messwerte \ innerhalb \ des \ Plateaus}$')
 plt.plot(x,m*x+b,'b-',label=r'$\mathrm{Ausgleichsfunktion}$')
 #plt.plot(300,0,'r.')
 plt.legend(loc='best')
 plt.xlabel(r'$Spannung \ \ U/V$')
-plt.ylabel(r'$Teilchenzahl   \ \ N/s $')
+plt.ylabel(r'$Teilchenzahl   \ \ N/s^{-1} $')
 plt.savefig('a).pdf')
 
 print('y-Achsenabschnitt B',B)
 print('steigung',M)
 print('plateau-steigung:',(M*400+B)/(M*300+B),)
-print('Fehler:',(m*100)/299.3)
+print('Fehler:',(M*100)/299.3)
 print('zeitlicher Abstand zwischen primär und nachentladungsimpulsen',
 '\n bei 700V in micro s',105,
 '\n bei 350V in micro s',120)
 print('Totzeit bei 700V mit ozill: in micro s',80 )
 
-n1=ufloat(195.2,np.sqrt(195.5))
-n12=ufloat(417.9,np.sqrt(417.9))
-n2=ufloat(236.0,np.sqrt(236.0))
+n1=ufloat(195.2,np.sqrt(1955)/10)
+n12=ufloat(417.9,np.sqrt(4179)/10)
+n2=ufloat(236.0,np.sqrt(2360)/10)
 print('n1=','{:.5u}'.format(n1))
 print('n2=','{:.5u}'.format(n2))
 print('n12=','{:.5u}'.format(n12))
@@ -65,3 +67,14 @@ deltaQ=delta_Q(I,N)
 
 #print('\n deltaQ \n',deltaQ)
 print('\n Q in -Q/e \n',deltaQ/const.e)
+
+deltaQe=deltaQ/const.e
+
+
+plt.figure(2)
+plt.errorbar(U ,noms(deltaQe*1e-9),xerr=0,yerr=stds(deltaQe*1e-9), fmt='cx')
+plt.plot(U,noms(deltaQe*1e-9),'bx',label=r'$\mathrm{Messwert}$')
+plt.legend(loc='best')
+plt.xlabel(r'$\mathrm{Spannung \ \ } U/V$')
+plt.ylabel(r'$\mathrm{Ladungen    \ \ }Q/10^{9}\mathrm{e} $')
+plt.savefig('plotIU.pdf')
