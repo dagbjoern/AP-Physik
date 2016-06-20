793008aa5178a28e2d296ec28a3b93c82a93d5e5
Dag-Björn Hering <dag.hering@udo.edu>
2016-06-20 10:03:28 +0200

    Teil der Auswertung von V504

--- V504/auswertung.py
+++ V504/auswertung.py
@@ -12,9 +12,7 @@ import scipy.constants as const
 
 # #Kennlinen
 U, I1 , I2 , I3 , I4 , I5 =np.genfromtxt('messung1.txt',unpack=True)
-# I_anlauf=np.genfromtxt('messung2.txt',unpack=True)
 
-#U=np.array([[0:5:60],[70:10:250]]
 U1=6.5
 I1_leucht=2.5
 U2=6
@@ -36,25 +34,105 @@ print(U)
 
 
 plt.figure(1)
-plt.plot(U,I1,'rx',label=r'$\mathrm{Messwerte für I=2,5}$')
-plt.plot(U,I2,'rx',label=r'$\mathrm{Messwerte für I=2,4}$')
-plt.plot(U,I3,'rx',label=r'$\mathrm{Messwerte für I=2,3}$')
-plt.plot(U,I4,'rx',label=r'$\mathrm{Messwerte für I=2,2}$')
-plt.plot(U,I5,'rx',label=r'$\mathrm{Messwerte für I=2,1}$')
+plt.plot(U,I1*1e6,'rx',label=r'$\mathrm{Messwerte \  für \ I=2,5}$')
+plt.plot(U,I2*1e6,'gx',label=r'$\mathrm{Messwerte \  für \ I=2,4}$')
+plt.plot(U,I3*1e6,'bx',label=r'$\mathrm{Messwerte \  für \ I=2,3}$')
+plt.plot(U,I4*1e6,'cx',label=r'$\mathrm{Messwerte \  für \ I=2,2}$')
+plt.plot(U,I5*1e6,'yx',label=r'$\mathrm{Messwerte \  für \ I=2,1}$')
 
 #plt.plot([100,100],[0,100],'b--',label=r'$\mathrm{Theorie}$')
 plt.legend(loc='best')
-plt.xlabel(r'$\mathrm{spannung U}$')
-plt.ylabel(r'$\mathrm{Bildweite \ b/mm}$')
+plt.xlabel(r'$\mathrm{Anodenspannung \ U/V}$')
+plt.ylabel(r'$\mathrm{Anodenstrom \ I/mA}$')
 plt.savefig('plot1.pdf')
 
 
-#plt.plot(x,N_D(x,*params_pb),'b-',label=r'$Ausgleichsfunktion$')
+#Fit für 3/2
+def V_23(V,b,a):
+    #c=(4/9)*const.epsilon_0*np.sqrt(2*const.e/const.m_e)*b
+    return(a*(V**b))
 
+U_fit=U[:np.size(U)-8]
+print(U_fit)
+I1_fit=I1[:np.size(U)-8]
+test=3/2
+params_1, covariance_1 =curve_fit(V_23,U_fit,I1_fit)
+errors_1 = np.sqrt(np.diag(covariance_1))
+print(params_1)
+print(errors_1)
+x=np.linspace(0,190)
+plt.figure(2)
+plt.plot(U,I1*1e6,'cx',label=r'$\mathrm{Messwerte \ außerhalb \ des \ Raumladungsgebietes} $')
+plt.plot(U_fit,I1_fit*1e6,'rx',label=r'$\mathrm{Messwerte \ innerhalb \ des\ Raumladungsgebietes }$')
+plt.plot(x,V_23(x,*params_1)*1e6,'b-',label=r'$\mathrm{Ausgleichsfunktion}$')
+plt.plot(U_fit,I1_fit*1e6,'rx')
+
+#plt.plot(x,V_23(x,3/2,400),'b-',label=r'$Ausgleichsfunktion$')
+
+#plt.plot([100,100],[0,100],'b--',label=r'$\mathrm{Theorie}$')
+plt.legend(loc='best')
+plt.xlabel(r'$\mathrm{Anodenspannung \ U/V}$')
+plt.ylabel(r'$\mathrm{Anodenstrom \ I/mA}$')
+plt.savefig('plotfitt.pdf')
+
+
+
+
+U_anlauf ,I_anlauf =np.genfromtxt('messung2.txt',unpack=True)
+
+I_anlauf=I_anlauf*1e-9
+
+r_i=1e6
+
+U_korr=U_anlauf-I_anlauf*r_i
+
+print('u_korr',U_korr)
+
+m , b , r ,p ,std =stats.linregress(U_korr,np.log(I_anlauf))
+
+def fehler_b(std_m,x):
+    return(std_m*np.sqrt(np.mean(x**2)))
+M=unp.uarray(m,std)
+B=unp.uarray(b,fehler_b(std,U_korr))
+
+x=np.linspace(0,1)
+plt.figure(3)
+plt.plot(U_korr,I_anlauf,'cx',label=r'$\mathrm{Messwerte}$')
+plt.plot(x,np.exp(m*x+b),'b-',label=r'$\mathrm{Ausgleichsfunktion}$')
+plt.yscale('log')
+#plt.plot(x,V_23(x,3/2,400),'b-',label=r'$Ausgleichsfunktion$')
+#plt.plot([100,100],[0,100],'b--',label=r'$\mathrm{Theorie}$')
+plt.legend(loc='best')
+plt.xlabel(r'$\mathrm{Gegenspannung \ U_k/V}$')
+plt.ylabel(r'$\mathrm{ln(I_A)}$')
+plt.savefig('plotanlauf.pdf')
+
+
+T=-const.e/(M*const.k)
+print('Steigung m',M)
+
+print(' b',B)
+print(T)
 #
-#
-# def fehler_b(std_m,x):
-#     return(std_m*np.sqrt(np.mean(x**2)))
-#
-#
-# Ab , Bb , rb ,pb ,stdb =stats.linregress(1+V,b_abbe)
+# U1=6.5
+# I1_leucht=2.5
+
+f=0.32
+o=5.7e-12
+n=0.28
+
+T_w=((U1*I1_leucht-0.9)/(f*n*o))**(1/4)
+print('T_w',T_w )
+
+#N_zu
+
+I_s=3.110
+
+
+Austritt_1=unp.log(I_s*(const.h**3)/(const.e*const.m_e*(const.k**2)*(T**2)))*const.k*T/const.e
+
+
+Austritt_2=np.log(I_s*(const.h**3)/(const.e*const.m_e*(const.k**2)*(T_w**2)))*const.k*T_w/const.e
+
+print('Austritt_1',Austritt_1)
+print('Austritt_2',Austritt_2)
